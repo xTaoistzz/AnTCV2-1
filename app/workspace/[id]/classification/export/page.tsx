b3eee84483a580be755d8b43343402e891608350
Your Name <PhuOfficial07@gmail.com>
2024-08-10 16:24:04 +0700

    Export Classification

---
+++ app/workspace/[id]/classification/export/page.tsx
@@ -0,0 +1,8 @@
+import ExportDataset from "@/app/components/annotation-props/export/classification_export";
+export default function Export(){
+    return (
+        <main>
+            <ExportDataset/>
+        </main>
+    )
+}
